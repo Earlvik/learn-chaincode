166dc7cab962e7d9302e704778b0124e9bcdb103
earlvik <earlviktor@gmail.com>
2017-05-11 15:53:13 +0200

    FitCoin Chaincode

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -19,13 +19,16 @@ package main
 import (
 	"errors"
 	"fmt"
-
+	"strconv"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
+
+var viktor = "viktor"
+var furqan = "furqan"
 
 // ============================================================================================================================
 // Main
@@ -42,7 +45,17 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
+	var err error
 
+	err = stub.PutState(viktor, []byte(strconv.Itoa(100)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = stub.PutState(furqan, []byte(strconv.Itoa(100)))
+	if err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
@@ -53,6 +66,34 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		var sender, receiver string
+		var err error
+
+		if len(args) != 3 {
+			return nil, errors.New("Incorrect number of arguments. Expecting 3. name of the sender, receiver and value to transfer")
+		}
+
+		sender = args[0] //rename for funsies
+		receiver = args[1]
+		amount, err := strconv.Atoi(args[2])
+
+		senderValue, err := stub.GetState(sender)
+		receiverValue, err := stub.GetState(receiver)
+		senderInt, err := strconv.Atoi(string(senderValue[:]))
+		receiverInt, err := strconv.Atoi(string(receiverValue[:]))
+
+		if err != nil {
+			return nil, err
+		}
+
+		err = stub.PutState(sender, []byte(strconv.Itoa(senderInt - amount)))
+		err = stub.PutState(receiver, []byte(strconv.Itoa(receiverInt + amount)))
+
+		if err != nil {
+			return nil, err
+		}
+		return nil, nil
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
@@ -62,11 +103,20 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
+	var key, jsonResp string
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+	if function == "read" {			
+		if len(args) != 1 {
+			return nil, errors.New("Incorrect number of arguments. Expecting name of the user to query")
+		}								//read a variable
+		key = args[0]
+		valAsbytes, err := stub.GetState(key)
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+		return valAsbytes, nil
 	}
 	fmt.Println("query did not find func: " + function)						//error
 
